2f3f4c818f4a3b867bd71b50f7bfdc3f41d418a1
Jack <jackeconomos@outlook.com>
2022-11-20 19:49:05 +1030

    update folders

--- routes/api/index.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const notesRoutes = require('./notesroutes');
-
-router.use(notesRoutes);
-
-module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,55 @@
-const express = require('express');
+const express = require("express");
+const fs = require("fs");
+const notes = require("./db/db.json");
+const path = require("path");
+const uuid = require("uuid");
+const { DH_CHECK_P_NOT_SAFE_PRIME } = require("constants");
+
+
 const app = express();
-const PORT = process.env.PORT || 3001;
+var PORT = process.env.PORT || 4023;
 
-const apiRoutes = require('./routes/api');
-const htmlRoutes = require('./routes/html');
-
-// parse POST data
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+//Setting routes for APIs
+//This gets notes saved and joins it in db.json
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"))
+});
 
-app.use(express.static('public'));
+// Post function to add new notes to db.json
+app.post("/api/notes", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const newNotes = req.body;
+    newNotes.id = uuid.v4();
+    notes.push(newNotes);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+    res.json(notes);
+});
 
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+//used for deleting notes
+app.delete("/api/notes/:id", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const delNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id);
+    fs.writeFileSync("./db/db.json", JSON.stringify(delNote));
+    res.json(delNote);
+})
+
+
+//HTML calls
+//calls home page
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+//call for notes.html
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+//Start listen
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
 });
