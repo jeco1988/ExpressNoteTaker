cd949f6904f3264636814a001c1758a2da7b099e
Jack <jackeconomos@outlook.com>
2022-11-20 19:08:24 +1030

    js update

--- routes/api/notesroutes.js
+++ routes/api/notesroutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
-const { notes } = require('../../db/db');
+const { notes } = require('../../db/db.json');
 const { createNewNote, findById, editNote, removeNote } = require('../../lib/notes');
 
 router.get('/notes', (req, res) => {
